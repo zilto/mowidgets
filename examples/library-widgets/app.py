1c2dafedd186145f2c3bd80033ab87225793bd95
zilto <68975210+zilto@users.noreply.github.com>
2025-09-11 11:19:35 -0400

    added dlt widget example

---
+++ examples/library-widgets/app.py
@@ -0,0 +1,104 @@
+import marimo
+
+__generated_with = "0.15.0"
+app = marimo.App(width="medium")
+
+with app.setup:
+    import marimo as mo
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(
+        r"""
+    # Loading external widgets
+
+    `mowidgets` makes it easy for libraries to distribute custom-built widgets for marimo notebooks.
+
+    This notebook shows how to use a widget packaged with the [dlt](https://github.com/dlt-hub/dlt) Python library. The widget allows to view the schema of loaded data.  
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(
+        r"""
+    ## Running a `dlt` pipeline
+    First, we run a `dlt` pipeline to ingest data from a REST API. This will produce data to render with our widget. 
+    """
+    )
+    return
+
+
+@app.cell
+def _():
+    import dlt
+    from dlt.sources.rest_api import rest_api_source
+    return dlt, rest_api_source
+
+
+@app.cell
+def _(dlt, rest_api_source):
+    jaffle_source = rest_api_source({
+        "client": {"base_url": "https://jaffle-shop.dlthub.com/api/v1"},
+        "resources": ["customers", "products", "stores"],
+    })
+
+    pipeline = dlt.pipeline("jaffle_shop", destination="duckdb")
+
+    pipeline.run(jaffle_source)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(
+        r"""
+    ## Use a dlt widget
+    Import `mowidgets` and retrieve the widget from `dlt.hepers.marimo.widgets`.
+    """
+    )
+    return
+
+
+@app.cell
+def _():
+    import mowidgets
+    from dlt.helpers.marimo.widgets import schema_viewer
+    return mowidgets, schema_viewer
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""As required, by `mowidgets.widgetize()`, the object `schema_viewer` is a `marimo.App` object. In other words, it represents an entire marimo notebook.""")
+    return
+
+
+@app.cell
+def _(schema_viewer):
+    type(schema_viewer)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""Call `mowidgets.widgetize()` on it to assign it to a variable. Use `await` in front of the object to render it! """)
+    return
+
+
+@app.cell
+def _(mowidgets, schema_viewer):
+    schema_viewer_widget = mowidgets.widgetize(schema_viewer)
+    return (schema_viewer_widget,)
+
+
+@app.cell
+async def _(schema_viewer_widget):
+    await schema_viewer_widget
+    return
+
+
+if __name__ == "__main__":
+    app.run()
